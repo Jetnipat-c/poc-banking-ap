8df4b5914f11df69e0c30f3409dffa41847f147c
Jetnipat-c <alonecheer@hotmail.com>
2023-05-26 16:45:39 +0700

    account service and delete data folder

--- banking-app/src/accounts/accounts.module.ts
+++ banking-app/src/accounts/accounts.module.ts
@@ -4,10 +4,15 @@ import { Account, AccountSchema } from './schemas/account.schema';
 import { AccountsController } from './accounts.controller';
 import { AccountsService } from './accounts.service';
 import { AccountsRepository } from './accounts.repository';
+import { RmqModule } from 'src/libs/common/rmq/rmq.module';
+import { ACCOUNTS_SERVICE } from './constants/services';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Account.name, schema: AccountSchema }]),
+    RmqModule.register({
+      name: ACCOUNTS_SERVICE,
+    }),
   ],
   controllers: [AccountsController],
   providers: [AccountsService, AccountsRepository],
--- banking-app/src/accounts/accounts.service.ts
+++ banking-app/src/accounts/accounts.service.ts
@@ -1,26 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { AccountsRepository } from './accounts.repository';
 import { CreateAccountDTO } from './dto/create-account.dto';
+import { ClientProxy } from '@nestjs/microservices';
+import { ACCOUNTS_SERVICE } from './constants/services';
 
 @Injectable()
 export class AccountsService {
-  constructor(private readonly accountsRepository: AccountsRepository) {}
+  constructor(
+    private readonly accountsRepository: AccountsRepository,
+    @Inject(ACCOUNTS_SERVICE) private accountsClient: ClientProxy,
+  ) {}
 
   async createAccount(createAccountDTO: CreateAccountDTO) {
-    let randomAccountNumber = '';
-    for (let i = 0; i < 10; i++) {
-      randomAccountNumber += Math.floor(Math.random() * 10).toString();
-    }
-    const docs = await this.accountsRepository.find({
-      accountNumber: randomAccountNumber,
-    });
-    if (docs.length > 0) {
-      return this.createAccount(createAccountDTO);
-    }
-
-    return this.accountsRepository.create({
-      ...createAccountDTO,
-      accountNumber: randomAccountNumber,
-    });
+    return this.accountsClient.emit('account_created', createAccountDTO);
   }
 }
